57f68d219f5685f9c64ad0cc0b407037d0a8738b
ADMIN <ADMIN@DESKTOP-NLIC8NT>
2020-11-27 22:32:55 -0500

    Datagrid

--- HotelAuditoria/reserva.aspx.designer.cs
+++ HotelAuditoria/reserva.aspx.designer.cs
@@ -103,6 +103,15 @@ namespace HotelAuditoria {
         protected global::System.Web.UI.WebControls.TextBox txtPrecio;
         
         /// <summary>
+        /// dgHabitaciones control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView dgHabitaciones;
+        
+        /// <summary>
         /// btnEnviar control.
         /// </summary>
         /// <remarks>
